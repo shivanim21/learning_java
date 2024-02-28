b134f6d7a63fe6c7eb7a5b446490e96eb232e4be
shivanim21 <shivanimaddala212@gmail.com>
2024-02-28 23:17:47 +0530

    Operators

---
+++ Demo.java
@@ -0,0 +1,13 @@
+public class Demo 
+{
+  public static void main(String args[]) {
+    int x = 6; //Assignment
+    int y = 5;
+    int a = 6;
+    int b = 9;
+
+    //boolean result =x < y; //Relational
+    boolean result = x>y && a<b; //Logical 
+    System.out.println(result); 
+  }
+}
