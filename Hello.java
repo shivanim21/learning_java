c7d11485f9961ee26eb0f690dd0f60664761509e
shivanim21 <shivanimaddala212@gmail.com>
2024-02-28 18:37:06 +0530

    assignment operator

--- Hello.java
+++ Hello.java
@@ -2,6 +2,29 @@ class Hello
 {
   public static void main(String a[])
   {
-    System.out.print("Hello World");
+    //System.out.print("Hello World");
+
+    //Variables
+    /*int num1 = 2;
+    int num2 = 1;
+    int result = num1 + num2;
+    System.out.println(result);*/
+
+    //Data Types
+    /*int num = 21;
+    byte by = 127;
+    short sh = 558;
+    long l = 5854l;
+
+    float f = 5.8f;
+    double d = 5.8;
+
+    char c = 's';
+
+    boolean b = true;*/
+
+    //Assignment operators
+    
+
   } 
 }
