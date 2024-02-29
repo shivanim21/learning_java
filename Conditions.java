667338014d7cea45a068b800486d0533c0432b91
shivanim21 <shivanimaddala212@gmail.com>
2024-02-29 23:46:22 +0530

    Conditionals

---
+++ Conditions.java
@@ -0,0 +1,63 @@
+public class Conditions
+{
+  public static void main(String args[]){
+    //if else
+    //int x = 8;
+    //int result = 0;
+    //int y = 6;
+    //int z = 9;
+    /*if(x>10 && x<=20)
+      System.out.println("Hello");
+    else
+      System.out.println("Bye");*/
+    
+    /*if(x>y)
+      System.out.println(x);
+    else
+      System.out.println(y);*/
+
+    // If else if
+    /*if(x>y && x>z)
+      System.out.println(x);
+    else if(y>z)
+      System.out.println(y);
+    else
+      System.out.println(z);*/
+
+      //Ternary Operator
+      /*result = x%2==0 ? 10 : 20;
+      System.out.println(result);*/
+
+      //Switch Statement
+      int n = 8;
+
+      switch(n){
+        case 1:
+          System.out.println("Monday");
+          break;
+        case 2:
+          System.out.println("Tuesday");
+          break;
+        case 3:
+          System.out.println("Wednesday");
+          break;
+        case 4:
+          System.out.println("Thursday");
+          break;
+        case 5:
+          System.out.println("Friday");
+          break;
+        case 6:
+          System.out.println("Saturday");
+          break;
+        case 7:
+          System.out.println("Sunday");
+          break;
+        default:
+          System.out.println("Not valid number");
+      }
+      
+
+
+  }
+}
